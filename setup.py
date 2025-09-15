61e57b2e523f7480c371f94c93bbbb4d836e9af4
Aansh Shah <aansh.shah@gmail.com>
2025-09-15 03:46:56 -0400

    fixes ci failures

--- setup.py
+++ setup.py
@@ -26,12 +26,11 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "numpy>=1.21.0",
         "pandas>=1.3.0",
